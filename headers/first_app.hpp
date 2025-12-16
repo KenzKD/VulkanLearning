799bf70dc5f5c455a3a1c0bc5dbd24288f74cf89
bobettes1 <kenzelkdsouza@gmail.com>
2025-12-16 16:06:28 +0400

    Finished Tutorial 2

--- headers/first_app.hpp
+++ headers/first_app.hpp
@@ -1,19 +1,25 @@
 #pragma once
+#define SHADER_PATH(x) "C:/Users/Lenovo/Desktop/Game_Making/Vulkan_Stuff/VulkanLearning/shaders/" x
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
 
-namespace lve 
+namespace lve
 {
 	class FirstApp
 	{
+	public:
+		static constexpr int WIDTH = 800;
+		static constexpr int HEIGHT = 600;
 
-		public:
-			static constexpr int WIDTH = 800;
-			static constexpr int HEIGHT = 600;
+		void run();
 
-			void run();
-
-		private:
-			LveWindow lveWindow{WIDTH, HEIGHT, "Vulkan window"};
+	private:
+		LveWindow lveWindow{WIDTH, HEIGHT, "Vulkan window"};
+		LvePipeline lve_pipeline
+		{
+			SHADER_PATH("simple_shader.vert.spv"),
+			SHADER_PATH("simple_shader.frag.spv")
+		};
 	};
 }
